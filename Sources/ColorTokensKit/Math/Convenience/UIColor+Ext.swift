371f539c73da6e80e931f055da7bde1b638c57d4
Siddhant Mehta <siddhant.olive92@gmail.com>
2024-07-29 12:08:59 -0400

    Moar comments

--- Sources/ColorTokensKit/Math/Convenience/UIColor+Ext.swift
+++ Sources/ColorTokensKit/Math/Convenience/UIColor+Ext.swift
@@ -8,7 +8,23 @@
 import Foundation
 import UIKit
 
+/// Extension to `UIColor` providing a method to convert `UIColor` to an `RGBColor`.
 public extension UIColor {
+    /**
+     Converts the `UIColor` to an `RGBColor` representation.
+     
+     - Returns: An `RGBColor` object if the conversion is successful, or `nil` if the conversion fails.
+     
+     This method extracts the red, green, blue, and alpha components from the `UIColor` and initializes an `RGBColor` with those values.
+     
+     Example:
+     ```swift
+     let uiColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1.0)
+     if let rgbColor = uiColor.rgbColor() {
+         print(rgbColor) // Output: RGBColor(r: 0.5, g: 0.5, b: 0.5, alpha: 1.0)
+     }
+     ```
+     */
     func rgbColor() -> RGBColor? {
         var r: CGFloat = 0
         var g: CGFloat = 0
