1a6ef568563f0f1a0423df0dc20cde0ef19d6bd4
Siddhant Mehta <siddhant.olive92@gmail.com>
2024-07-30 10:09:08 -0400

    Updated folder structure

--- Package.swift
+++ Package.swift
@@ -22,9 +22,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ColorTokensKit"),
-        .testTarget(
-            name: "ColorTokensKitTests",
-            dependencies: ["ColorTokensKit"]),
+            name: "ColorTokensKit")
     ]
 )
