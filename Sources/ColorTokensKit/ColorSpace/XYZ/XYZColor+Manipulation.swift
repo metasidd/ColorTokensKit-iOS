a2bfb31d8a068818cbc67cf9bff5c3e237b2fe1d
Siddhant Mehta <siddhant.olive92@gmail.com>
2025-02-22 16:30:08 +0530

    Remove extra method

--- Sources/ColorTokensKit/ColorSpace/XYZ/XYZColor+Manipulation.swift
+++ Sources/ColorTokensKit/ColorSpace/XYZ/XYZColor+Manipulation.swift
@@ -6,18 +6,5 @@
 import Foundation
 
 public extension XYZColor {
-    /// Creates a new XYZColor with adjusted components
-    func with(
-        x: CGFloat? = nil,
-        y: CGFloat? = nil,
-        z: CGFloat? = nil,
-        alpha: CGFloat? = nil
-    ) -> XYZColor {
-        return XYZColor(
-            x: x ?? self.x,
-            y: y ?? self.y,
-            z: z ?? self.z,
-            alpha: alpha ?? self.alpha
-        )
-    }
+    // TODO: Implement manipulation methods
 } 
