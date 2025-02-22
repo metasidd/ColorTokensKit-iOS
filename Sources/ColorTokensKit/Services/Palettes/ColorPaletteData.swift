497fb28a729f18d330eb111f2841365de2a65e1d
Siddhant Mehta <siddhant.olive92@gmail.com>
2025-02-22 15:23:48 +0530

    New folder structure

--- Sources/ColorTokensKit/Services/Palettes/ColorPaletteData.swift
+++ Sources/ColorTokensKit/Services/Palettes/ColorPaletteData.swift
@@ -1,15 +1,5 @@
 import Foundation
 
-struct ColorRamp {
-    let name: String
-    let stops: [String: LCHColor]
-
-    public init(name: String, stops: [String: LCHColor]) {
-        self.name = name
-        self.stops = stops
-    }
-}
-
 struct ColorPaletteData: Codable {
     private var palettes: [String: [String: String]]
 
